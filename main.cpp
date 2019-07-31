d76591b1d5fbc55f650464eb251498e19a449545
Alexandru OLOIERI <aoloieri@bitdefender.com>
2019-07-31 16:56:04 +0300

    moved all headers in "src ds-a" folder

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include "segment-tree-min-max.h"
-#include "disjoint-set-union.h"
-#include "fenwick-tree.h"
-#include "shortest-path-dijkstra.h"
-#include "minimum-spanning-tree-kruskal.h"
+#include "src ds-a/segment-tree-min-max.h"
+#include "src ds-a/disjoint-set-union.h"
+#include "src ds-a/fenwick-tree.h"
+#include "src ds-a/shortest-path-dijkstra.h"
+#include "src ds-a/minimum-spanning-tree-kruskal.h"
+
 using namespace std;
 
 int main()
